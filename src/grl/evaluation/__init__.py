2f117c19c7c992e7deef714766cc8ca1037e8111
Yucen Gao <41359719+GaoYucen@users.noreply.github.com>
2026-09-02 18:18:50 +0800

    Export conditional ranking evaluation API

--- src/grl/evaluation/__init__.py
+++ src/grl/evaluation/__init__.py
@@ -1,5 +1,14 @@
 from .spread import evaluate_baseline_method
 from .ranking import pairwise_accuracy, regression_ranking_metrics, top_k_recall
 from .sequential import evaluate_sequential_selector
+from .conditional_ranking import CandidateRankingGroup, evaluate_conditional_rankings
 
-__all__ = ["evaluate_baseline_method", "pairwise_accuracy", "regression_ranking_metrics", "top_k_recall", "evaluate_sequential_selector"]
+__all__ = [
+    "evaluate_baseline_method",
+    "pairwise_accuracy",
+    "regression_ranking_metrics",
+    "top_k_recall",
+    "evaluate_sequential_selector",
+    "CandidateRankingGroup",
+    "evaluate_conditional_rankings",
+]
