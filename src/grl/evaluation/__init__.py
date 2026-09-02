06de84ecffb5961b0af2cc403b2ef520b2632d54
Yucen Gao <41359719+GaoYucen@users.noreply.github.com>
2026-09-02 19:11:44 +0800

    Export uncertainty utilities

--- src/grl/evaluation/__init__.py
+++ src/grl/evaluation/__init__.py
@@ -2,6 +2,7 @@ from .spread import evaluate_baseline_method
 from .ranking import pairwise_accuracy, regression_ranking_metrics, top_k_recall
 from .sequential import evaluate_sequential_selector
 from .conditional_ranking import CandidateRankingGroup, evaluate_conditional_rankings
+from .uncertainty import PredictionSummary, conservative_order, summarize_ensemble
 
 __all__ = [
     "evaluate_baseline_method",
@@ -11,4 +12,7 @@ __all__ = [
     "evaluate_sequential_selector",
     "CandidateRankingGroup",
     "evaluate_conditional_rankings",
+    "PredictionSummary",
+    "conservative_order",
+    "summarize_ensemble",
 ]
