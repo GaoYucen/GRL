c5b35bb4ba1a5c0e13fe289d64b0d861db650aaf
Yucen Gao <41359719+GaoYucen@users.noreply.github.com>
2026-09-02 18:18:49 +0800

    Export conditional marginal dataset API

--- src/grl/training/__init__.py
+++ src/grl/training/__init__.py
@@ -1,4 +1,12 @@
 from .marginal_dataset import MarginalGainSample, build_marginal_dataset
 from .marginal_trainer import MarginalGainArtifacts, MarginalGainTrainer
+from .conditional_dataset import ConditionalMarginalGroup, build_conditional_marginal_dataset
 
-__all__ = ["MarginalGainSample", "build_marginal_dataset", "MarginalGainArtifacts", "MarginalGainTrainer"]
+__all__ = [
+    "MarginalGainSample",
+    "build_marginal_dataset",
+    "MarginalGainArtifacts",
+    "MarginalGainTrainer",
+    "ConditionalMarginalGroup",
+    "build_conditional_marginal_dataset",
+]
